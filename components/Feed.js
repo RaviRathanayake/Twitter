68e87783a85f454756edd1ed49615649509649c5
Ravi Rathnayake <ravirathnayakeal@gmail.com>
2022-07-18 12:14:55 +0530

    add the input section of the feed component

--- components/Feed.js
+++ components/Feed.js
@@ -1,4 +1,5 @@
 import { SparklesIcon } from "@heroicons/react/outline"
+import Input from "./Input"
 
 
 function Feed() {
@@ -9,10 +10,8 @@ function Feed() {
             <div className="hoverEffect flex items-center justify-center px-0 ml-auto w-9 h-9">
                 <SparklesIcon className="h-5"/>
             </div>
-
-
-            
-        </div>
+         </div>
+         <Input />
     </div>
   )
 }
